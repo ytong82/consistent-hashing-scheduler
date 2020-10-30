4e5ca4407ecdc9b75f4dbb2a426aeacefec8ac14
David Tong <ytong82@aliyun.com>
2020-10-30 06:31:53 -0500

    Virtual Node Consistent Hashing Algorithm

--- scheduler/src/main/java/interview/aliyun/scheduler/App.java
+++ scheduler/src/main/java/interview/aliyun/scheduler/App.java
@@ -1,13 +1,25 @@
 package interview.aliyun.scheduler;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.Map;
+import java.util.TreeMap;
+
+import interview.aliyun.scheduler.entity.TaskType;
+
+public class App {
+    public static void main( String[] args ) {
+    	Map<String, Integer> counter = new TreeMap<String, Integer>();
+        for (TaskType type : TaskType.values()) {
+        	counter.put(type.toString(), 0);
+        	if (type == TaskType.TypeC) {
+        		System.out.println(type);
+        		counter.put(type.toString(), 1);
+        	}
+        }
+        
+        counter.forEach(
+        	(key, value) -> {
+        		System.out.printf("key: %s, value: %s \n", key, value);
+        	}
+        );
     }
 }
