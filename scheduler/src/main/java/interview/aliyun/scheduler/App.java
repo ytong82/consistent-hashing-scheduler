f5bb61038eade15724c490e7a8b206bb94b37129
David Tong <ytong82@aliyun.com>
2020-11-01 16:40:18 -0600

    Working Version

--- scheduler/src/main/java/interview/aliyun/scheduler/App.java
+++ scheduler/src/main/java/interview/aliyun/scheduler/App.java
@@ -1,15 +1,40 @@
 package interview.aliyun.scheduler;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.text.DecimalFormat;
 import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 import interview.aliyun.scheduler.entity.Server;
-import interview.aliyun.scheduler.entity.Task;
 import interview.aliyun.scheduler.helper.ServerHelper;
 import interview.aliyun.scheduler.helper.TaskHelper;
 
 public class App {
+	private static void loadProperties() throws IOException {
+		InputStream input = App.class.getClassLoader().getResourceAsStream("config.properties");
+		Properties prop = new Properties();
+
+        if (input == null) {
+            System.out.println("Sorry, unable to find config.properties");
+            throw new IOException();
+        }
+        prop.load(input);
+	}
+	
     public static void main( String[] args ) {
+    	// load properties
+    	try {
+    		loadProperties();
+    	} catch (IOException ex) {
+    		ex.printStackTrace();
+    		System.exit(1);
+    	}
+    	
+    	// setup helpers
     	ServerHelper serverHelper = new ServerHelper();
     	TaskHelper taskHelper = new TaskHelper();
     	
@@ -17,19 +42,32 @@ public class App {
     	List<Server> servers = serverHelper.getServers();
     	Scheduler scheduler = new Scheduler(servers);
     	
-    	// start to schedule
-    	List<Task> tasks = taskHelper.getTasks();
-    	int count = 0;
-    	long startTime = System.currentTimeMillis();
-    	for (Task task : tasks) {
-    		count++;
-    		if (count % 10000 == 0) {
-    			long endTime = System.currentTimeMillis();
-    			System.out.printf("[RUN TIME] %s for [COUNT] %s \n", (endTime - startTime) + "ms", count);
-    			startTime = endTime;
-    		}
-    		scheduler.scheduleTask(task);
+    	// schedule
+    	ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(10);
+    	
+    	// start task runner at the interval of 5s
+    	for (int i=0; i<2; i++) {
+    		TaskRunner taskRunner = new TaskRunner(100000, 5, scheduler, taskHelper);
+    		executor.submit(taskRunner);
     	}
+    	
+    	// start task runner at the interval of 10s
+    	for (int i=0; i<2; i++) {
+    		TaskRunner taskRunner = new TaskRunner(100000, 10, scheduler, taskHelper);
+    		executor.submit(taskRunner);
+    	}
+    	
+    	// start task runner at the interval of 10s
+    	for (int i=0; i<2; i++) {
+    		TaskRunner taskRunner = new TaskRunner(100000, 60, scheduler, taskHelper);
+    		executor.submit(taskRunner);
+    	}
+    	try {
+    		executor.shutdown();
+			executor.awaitTermination(Long.MAX_VALUE, TimeUnit.MINUTES);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}   	
     	
     	// print result
     	long loadSum = 0;
